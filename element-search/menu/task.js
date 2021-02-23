b143d1c88eb97fea92ec873cc155280358bd800b
Roman Prilepskiy <r616on@gmail.com>
2021-02-23 21:25:47 +0300

    Home_works_2_2_Done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,18 @@
 
-let i = 0;
-const menuItem = document.querySelectorAll(".menu__item");
-for (let i = 0; i < menuItem.length; i++) {
-   menuItem[i].onclick = subMenuActiv;
-}
+const link = Array.from(document.querySelectorAll(".menu__link"));
+for (let i = 0; i < link.length; i++) {
+   if (link[i].nextElementSibling) {
+      link[i].onclick = function () {
+         let menu = this.closest(".menu_main");
+         menu = menu.querySelectorAll(".menu_sub")
+         for (let i = 0; i < menu.length; i++) {
+            menu[i].className = "menu menu_sub";
+         }
+         if (this.nextElementSibling.className === "menu menu_sub") {
+            this.nextElementSibling.className = "menu menu_sub  menu_active";
+         }
+         return false;
+      }
 
-function subMenuActiv() {
-
-   if (this.querySelector(".menu_sub")) {
-      clearMenuActiv();
-      this.querySelector(".menu_sub").className = ("menu menu_sub  menu_active")
-      return false;
-   }
-
-
-}
-function clearMenuActiv() {
-   const subMenu = document.querySelectorAll(".menu_sub");
-   for (let i = 0; i < subMenu.length; i++) {
-      subMenu[i].className = ("menu menu_sub")
    }
 }
-//console.log(menuItem);
