7555167431e652703826aa800091e9746ee89d83
Roman Prilepskiy <r616on@gmail.com>
2021-03-14 01:15:56 +0300

    Homeworks_documen_2_done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,23 @@
+const input = document.querySelector(".tasks__input");
+const add = document.querySelector(".tasks__add");
+const list = document.getElementById("tasks__list");
+
+add.addEventListener("click", addTasks);
+function addTasks(e) {
+   if (input.value.trim()) {
+      let child = document.createElement("div");
+      child.innerHTML = `<div class="task">
+      <div class="task__title">
+          ${input.value}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+      </div>`
+      const del = child.querySelector(".task__remove");
+      del.addEventListener("click", (e) => {
+         e.preventDefault();
+         const perent = e.target.closest(".task");
+         perent.remove();
+      })
+      list.append(child);
+   }
+}
