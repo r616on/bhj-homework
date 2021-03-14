6ea6bcea913acf6c9cbb44473ddb388d970f021b
Roman Prilepskiy <r616on@gmail.com>
2021-03-14 21:01:21 +0300

    Homeworks_strukture_3_done

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,7 +1,7 @@
 const control = Array.from(document.querySelectorAll(".product__quantity-control"));
 const productAdd = Array.from(document.querySelectorAll(".product__add"));
 const cartProducts = document.querySelector(".cart__products");
-
+const cart = document.querySelector(".cart");
 
 control.forEach((item) => item.addEventListener("click", (e) => {
    const element = e.target;
@@ -19,25 +19,32 @@ control.forEach((item) => item.addEventListener("click", (e) => {
 }));
 
 productAdd.forEach((item) => item.addEventListener("click", (e) => {
-   const element = e.target;
+   if (!(cart.classList.contains("cart-active"))) {
+      cart.classList.add("cart-active");
+   };
    const perentElement = e.target.closest(".product");
    const img = perentElement.querySelector(".product__image")
    const value = perentElement.querySelector(".product__quantity-value");
-
    const cartProductsArr = Array.from(document.querySelectorAll(".cart__product"));
    let index = cartProductsArr.findIndex((item) => +item.dataset.id === +perentElement.dataset.id)
-   console.log(index);
 
    if (index === (-1)) {
       let productNode = document.createElement('div');
-      productNode.innerHTML = `<div class="cart__product" data-id="${perentElement.dataset.id}">
-                           <img class="cart__product-image" src="${img.src}">
+      productNode.className = "cart__product";
+      productNode.dataset.id = perentElement.dataset.id;
+      productNode.innerHTML = `<img class="cart__product-image" src="${img.src}">
                            <div class="cart__product-count">${value.textContent}</div>
-                           </div>`;
+                           <div class="cart__remove">x</div>
+                           `;
+
+      productNode.querySelector(".cart__remove").addEventListener("click", (e) => {
+         const perentElement = e.target.closest(".cart__product");
+         perentElement.remove();
+      });
       cartProducts.append(productNode);
    } else {
       const presentCartCount = cartProductsArr[index].querySelector(".cart__product-count");
       presentCartCount.textContent = + presentCartCount.textContent + +value.textContent;
    }
 
-}))
+}));
