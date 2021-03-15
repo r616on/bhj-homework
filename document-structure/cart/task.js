1e7b8b674b15f7737b241373f9e303c168cf215d
Roman Prilepskiy <r616on@gmail.com>
2021-03-15 21:55:03 +0300

    Homeworks_Str_3_3_done

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -8,7 +8,7 @@ control.forEach((item) => item.addEventListener("click", (e) => {
    const perentElement = e.target.closest(".product__quantity-controls")
    const value = perentElement.querySelector(".product__quantity-value")
    if (element.classList.contains("product__quantity-control_dec")) {
-      if (+value.textContent > 0) {
+      if (+value.textContent > 1) {
          +value.textContent--;
       }
    };
@@ -25,28 +25,28 @@ productAdd.forEach((item) => item.addEventListener("click", (e) => {
    const cartProductsArr = Array.from(document.querySelectorAll(".cart__product"));
    let index = cartProductsArr.findIndex((item) => +item.dataset.id === +perentElement.dataset.id)
 
-   if (+value.textContent > 0) {
-      if (!(cart.classList.contains("cart-active"))) {
-         cart.classList.add("cart-active");
-      };
-      if (index === (-1)) {
-         let productNode = document.createElement('div');
-         productNode.className = "cart__product";
-         productNode.dataset.id = perentElement.dataset.id;
-         productNode.innerHTML = `<img class="cart__product-image" src="${img.src}">
+   if (!(cart.classList.contains("cart-active"))) {
+      cart.classList.add("cart-active");
+   };
+   if (index === (-1)) {
+      let productNode = document.createElement('div');
+      productNode.className = "cart__product";
+      productNode.dataset.id = perentElement.dataset.id;
+      productNode.innerHTML = `<img class="cart__product-image" src="${img.src}">
                            <div class="cart__product-count">${value.textContent}</div>
                            <div class="cart__remove">x</div>
                            `;
 
-         productNode.querySelector(".cart__remove").addEventListener("click", (e) => {
-            const perentElement = e.target.closest(".cart__product");
-            perentElement.remove();
-         });
-         cartProducts.append(productNode);
-      } else {
-         const presentCartCount = cartProductsArr[index].querySelector(".cart__product-count");
-         presentCartCount.textContent = + presentCartCount.textContent + +value.textContent;
-      }
+      productNode.querySelector(".cart__remove").addEventListener("click", (e) => {
+         const perentElement = e.target.closest(".cart__product");
+         perentElement.remove();
+      });
+      cartProducts.append(productNode);
+   } else {
+      const presentCartCount = cartProductsArr[index].querySelector(".cart__product-count");
+      presentCartCount.textContent = + presentCartCount.textContent + +value.textContent;
    }
 
 }));
+
+// Подскажите, мне кажется или этот скрипт заметно нагружает систему, и выполняется долго???
